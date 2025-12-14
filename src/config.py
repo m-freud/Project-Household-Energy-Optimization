0b1e1700af15e7c53830b72789f9130cc5a2bcb8
MFaFxF <max.freudenstein@protonmail.com>
2025-12-15 00:31:33 +0100

    better connections

--- src/config.py
+++ src/config.py
@@ -7,8 +7,13 @@ load_dotenv()
 class Config:
     ROOT_DIR = Path(__file__).parent.parent
 
-    SQL_PATH = ROOT_DIR / "sqlite" / "energy.db"
+    # data source
+    EXCEL_FILE_PATH = ROOT_DIR / "data" / "A.xlsx"
 
+    # sqlite
+    SQLITE_PATH = ROOT_DIR / "sqlite" / "energy.db"
+
+    # influx
     INFLUX_TOKEN = os.getenv("INFLUX_TOKEN", "")
     INFLUX_URL = os.getenv("INFLUX_URL", "")
     INFLUX_ORG = os.getenv("INFLUX_ORG", "")
