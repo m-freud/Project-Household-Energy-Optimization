2508a33ee1673b4b2556daf823245061d9ede3c6
MFaFxF <max.freudenstein@protonmail.com>
2026-01-05 13:34:35 +0100

    surplus paste

--- src/simulation/policies/greedy.py
+++ src/simulation/policies/greedy.py
@@ -125,4 +125,33 @@ def advanced_ev_bess(household:Household):
     # have to? absolutely have to vs just a good time:
     # do stuff when it makes sense. unless u have to. then do it because you have to.
     # decision tree to walk down the priority ladder
+    
+    # distribute suprlus first
+    if net_load < 0:
+        surplus = -net_load
 
+        # 1. charge evs first
+        evs_at_home = []
+        if household.ev1 and household.ev1.at_home:
+            evs_at_home.append('ev1')
+        if household.ev2 and household.ev2.at_home:
+            evs_at_home.append('ev2')
+
+        for ev in evs_at_home:
+            if ev.soc < requirements[ev]["soc"] * ev.capacity:
+                ev_power_available = surplus / len(evs_at_home)
+                ev_max_charge_power = min(
+                    ev.max_charge, # max charge rate if you need more than one step
+                    (ev.capacity - ev.soc) * 4 / ev.efficiency, # this is what is needed to get to 100% in one step.
+                    ev_power_available * 4 # distribute surplus evenly. 2kWh ? you can charge 8 kW for 15min
+                )
+                controls[f"{ev}_power"] = ev_max_charge_power
+                surplus -= ev_max_charge_power
+
+        # 2. charge bess if any surplus left
+        if surplus > 0 and household.bess:  
+            bess = household.bess
+            charge_power = min(surplus, bess.max_charge)
+            controls["bess_power"] = charge_power  # positive for charging
+    
+    return controls
--- src/simulation/requirements/charge_requirements.py
+++ src/simulation/requirements/charge_requirements.py
@@ -1,6 +1,21 @@
 # goal: minimize consumption.
 # requirements: this
 
+no_requirements = {
+    "ev1": {
+        "soc": 0.0,  # required state of charge (fraction of capacity)
+        "deadline": 0,  # timestep by which required_soc must be met
+    },
+    "ev2": {
+        "soc": 0.0,
+        "deadline": 0,
+    },
+    "bess": {
+        "soc": 0.0,  # required state of charge (fraction of capacity)
+        "deadline": 0,  # timestep by which required_soc must be met
+    },
+}
+
 
 basic_charge_requirements = {
     "ev1": {
