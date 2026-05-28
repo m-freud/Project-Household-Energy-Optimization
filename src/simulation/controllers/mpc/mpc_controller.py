96aaa453a31fa7caa931628057c1ca3deeb14fae
MFaFxF <max.freudenstein@protonmail.com>
2026-05-28 12:18:52 +0200

    foot: mpc controller something

--- src/simulation/controllers/mpc/mpc_controller.py
+++ src/simulation/controllers/mpc/mpc_controller.py
@@ -1,3 +1,5 @@
+import cvxpy as cp
+
 # paste this to enable src. imports
 
 from pathlib import Path
@@ -13,12 +15,38 @@ from src.simulation.household import Household
 
 
 class MPCController(BaseController):
-    def __init__(self, name: str, horizon: int = 24):
+    def __init__(self, name: str, horizon: int = 96):
         super().__init__(name)
         self.horizon = int(horizon)
 
-    def set_controls(self, household: Household, scenario: Scenario, *args, **kwargs):
+    def set_controls(self, household: Household, scenario: Scenario, predictor: str='oracle', *args, **kwargs):
         _ = (household, scenario, args, kwargs)
+
+        # get predictions for the next 24 hours
+        # make optimization problem and solve it
+
+        if predictor == 'oracle':
+            predictions = household.oracle_profiles.get("predictions", {})
+        else:
+            print('EEE EE  EEEEE EE EEEEEEEEEEEEEEE')
+            return
+
+
+        bess_power = cp.Variable(self.horizon, bounds=(-household.bess.max_discharge, household.bess.max_charge)) if household.bess else None
+        ev1_power = cp.Variable(self.horizon, bounds=(-household.ev1.max_discharge, household.ev1.max_charge)) if household.ev1 else None
+        ev2_power = cp.Variable(self.horizon, bounds=(-household.ev2.max_discharge, household.ev2.max_charge)) if household.ev2 else None
+
+        constraints = []
+
+
+
+
+
+
+
+
+
+
         return {
             "bess_power": 0.0,
             "ev1_power": 0.0,
