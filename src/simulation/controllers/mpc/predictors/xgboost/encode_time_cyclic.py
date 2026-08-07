ad62fd59316deda4b9f3c80fea8f514dcacece0e
MFaFxF <max.freudenstein@protonmail.com>
2026-08-07 10:38:25 +0200

    chore: cyclic time 1 param

--- src/simulation/controllers/mpc/predictors/xgboost/encode_time_cyclic.py
+++ src/simulation/controllers/mpc/predictors/xgboost/encode_time_cyclic.py
@@ -1,15 +1,16 @@
 import math
+from src.config import Config
 
-def encode_time_cyclic(timestep: int, total_timesteps: int) -> tuple[float, float]:
+def encode_time_cyclic(timestep: int) -> tuple[float, float]:
     """
     Encode a timestep as cyclic features (sine and cosine).
 
     Args:
         timestep (int): The current timestep to encode.
-        total_timesteps (int): The total number of timesteps in the cycle.
 
     Returns:
         tuple[float, float]: A tuple containing the sine and cosine values for the encoded timestep.
     """
+    total_timesteps = Config.TOTAL_TIMESTEPS_DAY
     angle = 2 * math.pi * (timestep / total_timesteps)
     return math.sin(angle), math.cos(angle)
