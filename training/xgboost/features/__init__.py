476019a91780cd92022bdbe14647c4ccc3c28456
MFaFxF <max.freudenstein@protonmail.com>
2026-08-11 17:39:22 +0200

    feat: initial model bank (untuned, untested)

---
+++ training/xgboost/features/__init__.py
@@ -0,0 +1,9 @@
+from training.xgboost.features.base_load_features import get_base_load_features
+from training.xgboost.features.pv_gen_features import get_pv_gen_features
+from training.xgboost.features.ev_status_features import get_ev_status_features
+
+__all__ = [
+    "get_base_load_features",
+    "get_pv_gen_features",
+    "get_ev_status_features",
+]
